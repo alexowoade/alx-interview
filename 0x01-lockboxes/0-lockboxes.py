744e32e0c84798e6a355e0c6cac1d0a3a90f8fdf
alexowoade <alexowoade@gmail.com>
2023-04-28 02:46:47 +0000

    alex

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 
+""" Lockboxes """
+
+
 def canUnlockAll(boxes):
-    """Determines if all boxes can be opened."""
-    keys = [0]  # keys already possessed
-    i = 0       # index of the box being checked
+    """
+    - boxes is a list of lists
+    - A key with the same number as a box opens that box
+    - You can assume all keys will be positive integers
+    - The first box boxes[0] is unlocked
+    - Return True if all boxes can be opened, else return False
+    """
+    canUnlockAll = False
+    keys = {0: True}
+    n_boxes = len(boxes)
+    while (True):
 
-    # check if all boxes can be unlocked
-    while i < len(keys):
-        for key in boxes[i]:
-            if key not in keys and key < len(boxes):
-                keys.append(key)
-        i += 1
+        n_keys = len(keys)
 
-    return len(keys) == len(boxes)
+        for i in range(len(boxes)):
+            if boxes[i] and keys.get(i, False):
+                for j in boxes[i]:
+                    if j < n_boxes:
+                        keys[j] = True
+                    boxes[i] = None
+
+        if not (len(keys) > n_keys):
+            break
+
+    if n_keys == len(boxes):
+        canUnlockAll = True
+
+    return canUnlockAll
 
